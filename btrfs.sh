42307acdf93b86a96db103da95732c43300fe461
bknightInfo <95592623+bknightInfo@users.noreply.github.com>
2022-04-03 19:30:26 +1000

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -10,19 +10,20 @@ cd /mnt
 btrfs subvolume create @
 btrfs subvolume create @home
 btrfs subvolume create @var
-btrfs subvolume create @data
+btrfs subvolume create @opt
+btrfs subvolume create @tmp
 btrfs subvolume create @srv
 
 cd
 umount /mnt
 
 mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/nvme0n1p3 /mnt
-mkdir -p /mnt/{boot/efi,home,var,data,src}
+mkdir -p /mnt/{boot,home,var,opt,tmp,src}
 mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /dev/nvme0n1p3 /mnt/var
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@data /dev/nvme0n1p3 /mnt/data
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@opt /dev/nvme0n1p3 /mnt/opt
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@tmp /dev/nvme0n1p3 /mnt/tmp
 mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@src /dev/nvme0n1p3 /mnt/srv
-
+mount -o subvol=@home /dev/nvme0n1p3 /mnt/var
 mount /dev/nvme0n1p1 /mnt/boot
 pacstrap /mnt base btrfs-progs git amd-ucode linux linux-firmware nano vim
 
