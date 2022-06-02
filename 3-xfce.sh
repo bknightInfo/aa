08105fe8f4961e6b2914bac9c42bd6ead3b93ec0
bknightInfo <95592623+bknightInfo@users.noreply.github.com>
2022-06-02 13:23:03 +1000

    Update 3-xfce.sh

--- 3-xfce.sh
+++ 3-xfce.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 
-git clone https://aur.archlinux.org/paru-bin
-cd paru-bin/
-makepkg -si --noconfirm
-cd
-rm -rf paru-bin
+# Variables
+country=AU
 
-sudo pacman -S --noconfirm --needed reflector
-sudo reflector -c AU -a 12 --sort rate --save /etc/pacman.d/mirrorlist --verbose
+# Options
+aur_helper=true
+install_ly=true
+gen_xprofile=false
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+sudo reflector -c $country -a 12 --sort rate --save /etc/pacman.d/mirrorlist --verbose
 sudo pacman -Syu
+
+if [[ $aur_helper = true ]]; then
+    cd /tmp
+    git clone https://aur.archlinux.org/paru.git
+    cd paru/;makepkg -si --noconfirm;cd
+fi
+
 
 # install x essentials
 sudo pacman -S --noconfirm --needed xorg-server 
@@ -28,8 +38,6 @@ sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter
 sudo pacman -S ttf-dejavu ttf-liberation ttf-anonymous-pro terminus-font ttf-font-awesome
 paru -S --noconfirm ttf-ms-fonts noto-fonts ttf-roboto ttf-inconsolata
 
-# Install admin tools
-paru -S --noconfirm pamac-aur
 
 # install dev tools
 sudo pacman -S --noconfirm stow zsh zsh-completions 
@@ -39,10 +47,6 @@ paru -S --noconfirm visual-studio-code-bin
 # install useful apps
 sudo pacman -S --noconfirm vlc unrar neofetch htop alacritty firefox
 paru -S --noconfirm  alacritty-themes timeshift timeshift-autosnap libreoffice-fresh numlockx unimatrix
-
-#echo "change shell"
-#chsh --shell /bin/zsh bknight2k
-#curl -sS https://starship.rs/install.sh | sh
 
 # wallpapers
 git clone --recursive https://github.com/bknightInfo/wallpapers ~/.local/share/wallpapers
